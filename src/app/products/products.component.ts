f969c6a1d158beafe01b2e09e54b578c8643d683
solomonilori <solomonilori@gmail.com>
2023-02-14 16:21:21 +0100

    public property declared

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -7,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  myProducts = ['Shoes','Shirts','Wrist Watches'];
+  @Input() myProducts: string[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
