8794835e68053cf2897f0b6973d7ad3a60f28cd7
dev3Nigerian <faithfulodigbo@gmail.com>
2023-02-16 02:33:20 +0100

    added necessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
+import { AdJobComponent } from './ad-job.component';
+import { AdprofileComponent } from './ad-profile.component';
+import { AdbannerComponent } from './ad-banner.compoment';
+import { AdDirective } from './ad.directive';
+import { AdService } from './ad.service';
 
 @NgModule({
   declarations: [
